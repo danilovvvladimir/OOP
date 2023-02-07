c4a4701b283200d2a4936d9df057f6788a9cc9a2
Fruity <fruityflunt@gmail.com>
2023-02-07 19:27:26 +0300

    replace & invert переделал

--- lab1/1_replace/1_replace/Source.cpp
+++ lab1/1_replace/1_replace/Source.cpp
@@ -1,11 +1,125 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
-using namespace std;
 
-// replace
+
+//==2==Чтение текста из IN файла построчно. (открыты как IN, так и OUT файлы).
+//==1== Замена подстроки в строке
+//	- Нахождение количества вхождений в строку подстроки.
+//	- Циклом с количеством итераций, найденных выше:
+//		- Ищем эту подстроку(первый индекс) (find - даёт индекс);
+//		- До этого индекса строку засовываю в stringFirstHalf, после этого индекса+длины подстроки начальной до конца в stringSecondHalf;
+//		- Склеиваю stringFirstHalf + новая подстрока + stringSecondHalf;
+//		//и так в цикле, пока не будут изменены все вхождения !!!!!!!!!!!!!!подумать про многократное вхождение строки (контрится указанием индекса с которого будет идти цикл)
+//==3==Вывод этой строки в OUT файл.
+//
+// Разбиение на функции.
+// Обработка ошибок.
+//
+//
+// .find(substring, INDEXofstart)!!!!!!!!!!!!!!!!!!!!!
+
+int count_of_substrings(std::string src, std::string sub) {
+	int start = 0;
+	int count = 0;
+	int pos = 0;
+	for (;;) {
+		pos = src.find(sub.c_str(), start);
+		if (pos != -1) {
+			start = pos + sub.size();
+			count++;
+		}
+		else {
+			break;
+		}
+	}
+	return count;
+}
+
+std::string replaceSubstring(std::string mainStr, std::string substrFirst, std::string substrSecond)
+{
+	std::string strResult = mainStr;
+	int lengthSubstringNormal = substrFirst.size();
+	int lengthSubstringNeeded = substrSecond.size();
+	int indexOfStart = 0;
+	int countSubstring = count_of_substrings(strResult, substrFirst);
+
+	if (countSubstring > 0)
+	{
+		for (int k = 0; k < countSubstring; k++)
+		{
+			int lengthStrIN = strResult.size();
+			std::string stringFirstHalf = "";
+			std::string stringSecondHalf = "";
+			int temp = strResult.find(substrFirst, indexOfStart);
+			//std::cout << "indexOfStart: " << indexOfStart << std::endl;
+			//std::cout << "indexOfSubstringStart: " << temp << std::endl;
+			indexOfStart = temp + lengthSubstringNeeded;
+
+
+			for (int i = 0; i < lengthStrIN; i++)
+			{
+
+				if (i < temp)
+				{
+					stringFirstHalf += strResult[i];
+				}
+				else
+				{
+					if (i >= temp + lengthSubstringNormal)
+					{
+						stringSecondHalf += strResult[i];
+					}
+				}
+			}
+			strResult = stringFirstHalf + substrSecond + stringSecondHalf;
+		}
+	}
+	return strResult;
+}
 
 int main()
 {
+	std::string substringNormal = "1231234";
+	std::string substringNeeded = "NOT NEEDLE";
+
+
+	std::ifstream fIN;
+	std::ofstream fOUT;
+
+	std::string fINpath = "in.txt";
+	std::string fOUTpath = "out.txt";
+
+	fIN.open(fINpath);
+	if (fIN.is_open())
+	{
+		fOUT.open(fOUTpath);
+		if (fOUT.is_open())
+		{
+			while (!fIN.eof())
+			{
+				std::string line = "";
+				std::getline(fIN, line);
+				line = replaceSubstring(line, substringNormal, substringNeeded);
+				fOUT << line << std::endl;
+
+			}
+			fOUT.close();
+			fIN.close();
+		}
+		else
+		{
+			std::cout << "OUT File couldn't been opened" << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		std::cout << "IN File couldn't been opened" << std::endl;
+		return 1;
+	}
+
 
 	return 0;
 }
--- lab1/3_invert/3_invert/Source.cpp
+++ lab1/3_invert/3_invert/Source.cpp
@@ -3,12 +3,140 @@
 #include <string>
 #include <iomanip> 
 
-using namespace std;
 
-string transformDoubleToStringAndRound(double num)
+// Функция нахождения определителя в 3*3 матрице
+// Функция нахождения определителя в 2*2 матрице
+// Функция нахождения союзной матрицы
+// Функция нахождения транпонированной матрицы
+// Главная функция нахождения обратной матрицы (союзная-транспонированная * 1/определитель 3*3)
+// 
+//
+//
+//
+
+double findTripleMatrixDeterminant(double matrix[][3])
 {
-	string result;
-	string numStr = to_string(num);
+	double determinant = ((matrix[0][0] * matrix[1][1] * matrix[2][2]) + (matrix[0][2] * matrix[1][0] * matrix[2][1]) + (matrix[0][1] * matrix[1][2] * matrix[2][0]) -
+		(matrix[0][2] * matrix[1][1] * matrix[2][0]) + (matrix[0][1] * matrix[1][0] * matrix[2][2]) + (matrix[0][0] * matrix[1][2] * matrix[2][1]));
+	return determinant;
+}
+
+
+double findDoubleMatrixDeterminant(double matrix[][2])
+{
+	double determinant = ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0] ));
+	return determinant;
+}
+
+void findAdjugateMatrix(double matrix[][3])
+{
+	//double ** AdjugateMatrix = new double* [3];
+	double AdjugateMatrix[3][3];
+	double minorAdjugateMatrix[2][2];
+
+	bool isMinus = false;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (i == 0 && j == 0)
+			{
+				minorAdjugateMatrix[0][0] = matrix[1][1];
+				minorAdjugateMatrix[0][1] = matrix[1][2];
+				minorAdjugateMatrix[1][0] = matrix[2][1];
+				minorAdjugateMatrix[1][1] = matrix[2][2];
+
+			}
+			if (i == 0 && j == 1)
+			{
+				minorAdjugateMatrix[0][0] = matrix[1][0];
+				minorAdjugateMatrix[0][1] = matrix[1][2];
+				minorAdjugateMatrix[1][0] = matrix[2][0];
+				minorAdjugateMatrix[1][1] = matrix[2][2];
+
+			}
+			if (i == 0 && j == 2)
+			{
+				minorAdjugateMatrix[0][0] = matrix[1][0];
+				minorAdjugateMatrix[0][1] = matrix[1][1];
+				minorAdjugateMatrix[1][0] = matrix[2][0];
+				minorAdjugateMatrix[1][1] = matrix[2][1];
+
+			}
+			//
+			if (i == 1 && j == 0)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][1];
+				minorAdjugateMatrix[0][1] = matrix[0][2];
+				minorAdjugateMatrix[1][0] = matrix[2][1];
+				minorAdjugateMatrix[1][1] = matrix[2][2];
+
+			}
+			if (i == 1 && j == 1)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][0];
+				minorAdjugateMatrix[0][1] = matrix[0][2];
+				minorAdjugateMatrix[1][0] = matrix[2][0];
+				minorAdjugateMatrix[1][1] = matrix[2][2];
+
+			}
+			if (i == 1 && j == 2)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][0];
+				minorAdjugateMatrix[0][1] = matrix[0][1];
+				minorAdjugateMatrix[1][0] = matrix[2][0];
+				minorAdjugateMatrix[1][1] = matrix[2][1];
+
+			}
+			//
+			if (i == 2 && j == 0)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][1];
+				minorAdjugateMatrix[0][1] = matrix[0][2];
+				minorAdjugateMatrix[1][0] = matrix[1][1];
+				minorAdjugateMatrix[1][1] = matrix[1][2];
+
+			}
+			if (i == 2 && j == 1)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][0];
+				minorAdjugateMatrix[0][1] = matrix[0][2];
+				minorAdjugateMatrix[1][0] = matrix[1][0];
+				minorAdjugateMatrix[1][1] = matrix[1][2];
+
+			}
+			if (i == 2 && j == 2)
+			{
+				minorAdjugateMatrix[0][0] = matrix[0][0];
+				minorAdjugateMatrix[0][1] = matrix[0][1];
+				minorAdjugateMatrix[1][0] = matrix[1][0];
+				minorAdjugateMatrix[1][1] = matrix[1][1];
+
+			}
+
+			AdjugateMatrix[i][j] = findDoubleMatrixDeterminant(minorAdjugateMatrix);
+			if (isMinus)
+			{
+				AdjugateMatrix[i][j] = -1 * AdjugateMatrix[i][j];
+			}
+			isMinus = !isMinus;
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int k = 0; k < 3; k++)
+		{
+			std::cout << AdjugateMatrix[i][k] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	//return AdjugateMatrix;
+}
+
+std::string transformDoubleToStringAndRound(double num)
+{
+	std::string result;
+	std::string numStr = std::to_string(num);
 	int length = numStr.size();
 	int counter = 0;
 	for (int i = 0; i < length; i++)
@@ -21,67 +149,74 @@ string transformDoubleToStringAndRound(double num)
 		{
 			counter++;
 		}
-			result += numStr[i];
+		result += numStr[i];
 	}
 	return result;
 }
 
 int main()
 {
-	string fInPath = "in.txt";
-
-	ifstream fIn;
+	system("chcp 1251> nul");
+	std::string fInPath = "in.txt";
+	std::ifstream fIn;
 	fIn.open(fInPath);
 
-	int matrixIn[3][3];
-	string matrixFloatIn[3][3];
+	double matrixIn[3][3];
+	//string matrixFloatIn[3][3];
+
 	if (fIn.is_open())
 	{
 		for (int i = 0; i < 3; i++)
 		{
 			for (int k = 0; k < 3; k++)
 			{
-				int tempNumber;
+				// Запись матрицы в двумерный массив.
+				double tempNumber;
 				fIn >> tempNumber;
 				matrixIn[i][k] = tempNumber;
 			}
 		}
-		for (int i = 0; i < 3; i++)
+		double determinant = findTripleMatrixDeterminant(matrixIn);
+		if (determinant == 0)
 		{
-			for (int k = 0; k < 3; k++)
-			{
-				double tempNumber;
-				tempNumber = (1.0 / matrixIn[i][k]);
-				tempNumber = round(1000 * tempNumber) / 1000.0;
-				matrixFloatIn[i][k] = transformDoubleToStringAndRound(tempNumber);
-				//matrixFloatIn[i][k] = 1.0 / matrixIn[i][k];
-			}
+
+			std::cout << "Детерминант матрицы равен нулю, обратной матрицы не существует." << std::endl;
 		}
+		else
+		{
+			std::cout << "Детерминант матрицы: " << determinant << std::endl << std::endl;
+
+		}
+		std::cout << std::endl;
+		findAdjugateMatrix(matrixIn);
+		std::cout << std::endl;
+		std::cout << std::endl;
+		//for (int i = 0; i < 3; i++)
+		//{
+		//	for (int k = 0; k < 3; k++)
+		//	{
+		//		double tempNumber;
+		//		tempNumber = (1.0 / matrixIn[i][k]);
+		//		tempNumber = round(1000 * tempNumber) / 1000.0;
+		//		matrixFloatIn[i][k] = transformDoubleToStringAndRound(tempNumber);
+		//		//matrixFloatIn[i][k] = 1.0 / matrixIn[i][k];
+		//	}
+		//}
 
 
 	}
 	else
 	{
-		cout << "Couldn't open the file" << endl;
+		std::cout << "Couldn't open the file" << std::endl;
 	}
 
 	for (int i = 0; i < 3; i++)
 	{
 		for (int k = 0; k < 3; k++)
 		{
-			cout << matrixIn[i][k] << "\t";
+			std::cout << matrixIn[i][k] << "\t";
 		}
-		cout << endl;
-	}
-	cout << "==============" << endl;
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int k = 0; k < 3; k++)
-		{
-			cout << matrixFloatIn[i][k] << "\t";
-		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 	return 0;
 }
