280c3ececac83b95306019e9ea53c18c2f042f8b
Fruity <fruityflunt@gmail.com>
2023-02-09 08:20:04 +0300

    доделал radix, осталось только проверка на английские символы

--- lab1/2_radix/2_radix/Source.cpp
+++ lab1/2_radix/2_radix/Source.cpp
@@ -1,13 +1,15 @@
 #include <iostream> 
 #include <string> 
 
+
 std::string numeralSystemSymbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+bool wasError = false;
 
 std::string reverseString(std::string &str)
 {
 	std::string result = str;
 	char temp;
-	for (int i = 0; i < result.size() / 2; i++)
+	for (int i = 0; i < static_cast<int>(result.size() / 2); i++)
 	{
 		temp = result[i];
 		result[i] = result[result.size() - 1 - i];
@@ -17,21 +19,54 @@ std::string reverseString(std::string &str)
 	return result;
 }
 
-//int StringToInt(const string& str, int radix, bool & wasError);
-int stringToInt(const std::string& str, int startedNumeralSystem)
+int stringToInt(const std::string& str, int startedNumeralSystem, bool & wasError)
 {
+	std::string intMaxString = "2147483647";
+	std::string intMinString = "-2147483647";
+
+	std::string myString = str;
+	if (myString[0] != '-')
+	{
+		if (intMaxString.size() <= myString.size())
+		{
+			if (intMaxString < myString)
+			{
+				wasError = true;
+				return 1;
+			}
+
+		}
+	}
+	else
+	{
+		if (intMinString.size() <= myString.size())
+		{
+			if (intMinString < myString)
+			{
+				wasError = true;
+				return 2;
+			}
+
+		}
+	}
+
 	int result = 0;
 	int digit[91] = { 0 };
+
+
 
 	for (int i = 0; numeralSystemSymbols[i]; i++)
 	{
 		digit[numeralSystemSymbols[i]] = i;
 	}
 
-	
-	// Иду по всем символам введённой строки и создаю число в десятичной СС, как многочлен.
 	for (int i = 0; str[i]; i++)
 	{
+		if (digit[str[i]] >= startedNumeralSystem || (digit[str[i]] == 0 && (str[i] != '0' && str[i] != '-')))
+		{
+			wasError = true;
+			return 3;
+		}
 		result = result * startedNumeralSystem + digit[str[i]];
 	}
 
@@ -44,11 +79,15 @@ int stringToInt(const std::string& str, int startedNumeralSystem)
 	return result;
 }
 
-//std::string IntToString(int n, int radix, bool & wasError);
-std::string intToString(int resInt, int finalNumeralSystem)
+std::string intToString(int resInt, int finalNumeralSystem, bool & wasError)
 {
 	std::string result;
 	bool isNegative = false;
+
+	if (resInt == 0)
+	{
+		return "0";
+	}
 
 	if (resInt < 0)
 	{
@@ -70,30 +109,52 @@ std::string intToString(int resInt, int finalNumeralSystem)
 	return result;
 }
 
-int main()
+void printResult(std::ostream& output, const std::string& strResult)
 {
+	output << "Final: " <<strResult << "\n";
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	// Русские буквы поправить, ограничение только что б английские использовались
 	system("chcp 1251 > nul");
-	std::string result = "";
-	// Проверки на максимальные и минимальные значения (в 16-ричной системе Z быть не может при вводе)
-	// wasERROR как в docs при перенаполнении
-	int startedNumeralSystem;
-	int finalNumeralSystem;
-	std::string s;
-	std::cout << "startedNumeralSystem: ";
-	std::cin >> startedNumeralSystem;
-	std::cout << std::endl;
-	std::cout << "finalNumeralSystem: ";
-	std::cin >> finalNumeralSystem;
-	std::cout << std::endl;
-	std::cout << "Number: ";
-	std::cin >> s;
-	std::cout << std::endl;
+	if (argc != 4)
+	{
+		std::cout << "Invalid argument count\n"
+			<< "Usage: radix.exe <source notation> <destination notation> <value>\n";
+		return 1;
+	}
 
-	int intResult = (stringToInt(s, startedNumeralSystem));
-	std::cout << intResult << std::endl;
+	int startedNumeralSystem = atoi(argv[1]);
+	int finalNumeralSystem = atoi(argv[2]);
+	std::string value = argv[3];
 
-	std::string strResult = intToString(intResult, finalNumeralSystem);
-	std::cout << strResult << std::endl;
+	int intResult = (stringToInt(value, startedNumeralSystem, wasError));
+
+	if (wasError)
+	{
+		if (intResult == 1)
+		{
+			std::cout << "Ошибка. Ввёденное число больше максимально допустимого." << std::endl;
+			return 1;
+		}
+		if (intResult == 2)
+		{
+			std::cout << "Ошибка. Ввёденное число меньше минимально допустимого." << std::endl;
+			return 1;
+		}
+		if (intResult == 3)
+		{
+			std::cout << "Ошибка. Это число не входит в данную систему счисления." << std::endl;
+			return 1;
+		}
+		
+	}
+
+	std::string strResult = intToString(intResult, finalNumeralSystem, wasError);
+	printResult(std::cout, strResult);
 
 	return 0;
 }
