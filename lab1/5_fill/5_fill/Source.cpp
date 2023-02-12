f687061b1568b16f302fa8982b52afd20f0f0947
Fruity <fruityflunt@gmail.com>
2023-02-12 20:22:51 +0300

    fill: finally worked correctly w/ \n and overflow & tests

--- lab1/5_fill/5_fill/Source.cpp
+++ lab1/5_fill/5_fill/Source.cpp
@@ -4,140 +4,143 @@
 #include <queue>
 #include <vector>
 
-using namespace std;
+const char CH_SPACE = ' ';
+const char CH_SPACE2 = '_';
+const char CH_DOT = '.';
+const char CH_WALL = '#';
+const char CH_FILL_POINT = 'O';
 
 const int MAX_SIZE = 100;
 const int directionX[4] = { 1, -1, 0, 0 };
 const int directionY[4] = { 0, 0, 1, -1 };
-char field[MAX_SIZE][MAX_SIZE]{ 0 };
 
 struct Point
 {
 	int x, y;
 };
+std::vector<Point> startPoints;
+char field[MAX_SIZE][MAX_SIZE]{ 0 };
 
-queue<Point> q;
+std::queue<Point> q;
+
 void exploreNeighbours(Point p)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		Point ttPoint = p;
-		ttPoint.x += directionX[i];
-		ttPoint.y += directionY[i];
-		if (ttPoint.x < 0 || ttPoint.x > MAX_SIZE - 1)
+		Point neighbourPoint = p;
+		neighbourPoint.x += directionX[i];
+		neighbourPoint.y += directionY[i];
+		if (neighbourPoint.x < 0 || neighbourPoint.x > MAX_SIZE - 1)
 			continue;
-		if (ttPoint.y < 0 || ttPoint.y > MAX_SIZE - 1)
+		if (neighbourPoint.y < 0 || neighbourPoint.y > MAX_SIZE - 1)
 			continue;
-		if (field[ttPoint.x][ttPoint.y] == '#')
+		if (field[neighbourPoint.x][neighbourPoint.y] == CH_WALL)
 			continue;
-		if (field[ttPoint.x][ttPoint.y] == 'O')
+		if (field[neighbourPoint.x][neighbourPoint.y] == CH_FILL_POINT)
 			continue;
-		if (field[ttPoint.x][ttPoint.y] == '.')
+		if (field[neighbourPoint.x][neighbourPoint.y] == CH_DOT)
 			continue;
 
-		field[ttPoint.x][ttPoint.y] = '.';
-		q.push(ttPoint);
+		field[neighbourPoint.x][neighbourPoint.y] = CH_DOT;
+		q.push(neighbourPoint);
 	}
 }
 
 void showField(std::ostream& output)
 {
-	//system("cls");
 	for (int i = 0; i < MAX_SIZE; i++)
 	{
 		for (int j = 0; j < MAX_SIZE; j++)
 		{
 			output << field[i][j];
 		}
-		output << endl;
+		output << std::endl;
 	}
-	//Sleep(20);
 }
-
-int main()
+void validateField(std::ifstream& inputFile)
 {
-	// Игнорирование символов за MAX_SIZE, делается так: если на крайнем J не \n, то пока не будет встречен конец строки inputFile.get(ch);
-	// Вынесение по функциям, validateFile, fill, showField на printField переименовать
-	// Константы для символов
-	// argc, argv
-	vector<Point> startPoints;
-	ifstream inputFile;
-	inputFile.open("emp.txt");
-	if (!inputFile.is_open())
-	{
-		cout << "FILE not opened" << endl;
-		return 1;
-	}
-
-	bool isEnd = false;
-	bool isN = false;
-	bool isAddN = false;
-
+	bool markerEndLine = false;
+	bool markerLoop = false;
+	bool markerEndLineLoop = false;
 
 	char ch;
 	for (int i = 0; i < MAX_SIZE; i++)
 	{
 		for (int j = 0; j < MAX_SIZE; j++)
 		{
-			ch = ' ';
-			if (!isN && !isAddN)
+
+			ch = CH_SPACE;
+			if (markerEndLineLoop)
+			{
+				ch = CH_SPACE2;
+				while (ch != '\n')
+				{
+					inputFile.get(ch);
+					if (ch == CH_SPACE2)
+					{
+						ch = '\n';
+					}
+				}
+				markerEndLine = false;
+				markerEndLineLoop = false;
+			}
+
+			if (markerLoop)
+			{
+				if (j == MAX_SIZE - 1)
+				{
+					markerLoop = false;
+				}
+				field[i][j] = ch;
+				continue;
+			}
+			else
 			{
 				inputFile.get(ch);
 			}
-			if (isAddN)
-			{
-				ch = '\n';
-				isAddN = false;
-				isEnd = true;
-			}
+
 			if (j == MAX_SIZE - 1)
 			{
-				isEnd = true;
-				if (isN)
-				{
-					isN = false;
-					ch = ' ';
-					isAddN = true;
-				}
+				markerEndLine = true;
 			}
 
-			if (isN && !isEnd)
+			if (ch == '\n')
 			{
-				field[i][j] = ' ';
-				continue;
-			}
-			if (ch != '\n')
-			{
-				if (!isN)
+				if (!markerEndLine)
 				{
-					field[i][j] = ch;
+					markerLoop = true;
+					j--;
+					continue;
+				}
+				else
+				{
+					j--;
+					continue;
 				}
 			}
 			else
 			{
-				if (j == 0 && isEnd)
+				if (markerEndLine)
 				{
-					j--;
-					isEnd = false;
+					markerEndLineLoop = true;
+					field[i][j] = ch;
 				}
 				else
 				{
-
-					isN = true;
-					field[i][j] = ' ';
-					if (j == MAX_SIZE - 1)
-					{
-						isN = false;
-					}
+					field[i][j] = ch;
 				}
 			}
-			if (ch == 'O')
+
+			if (ch == CH_FILL_POINT)
 			{
 				startPoints.push_back(Point{ i, j });
 			}
 		}
 	}
-	//showField(cout);
+}
+
+void fillField()
+{
 	if (startPoints.size() > 0)
 	{
 		for (int i = 0; i < static_cast<int>(startPoints.size()); i++)
@@ -151,18 +154,59 @@ int main()
 					Point tempP = q.front();
 					q.pop();
 					exploreNeighbours(tempP);
-					//showField(cout);
 				}
 			}
 		}
 	}
-	inputFile.close();
-	ofstream outputFile;
-	outputFile.open("out.txt");
-	showField(outputFile);
-	//showField(cout);
+}
 
-	outputFile.close();
+int HandleFill(std::string inputFilePath, std::string outputFilePath)
+{
+	std::ifstream inputFile;
+	inputFile.open(inputFilePath);
+
+	if (inputFile.is_open())
+	{
+		validateField(inputFile);
+		fillField();
+		std::ofstream outputFile;
+		outputFile.open(outputFilePath);
+		if (outputFile.is_open())
+		{
+
+			showField(outputFile);
+		}
+		else
+		{
+			std::cout << "OUTFILE not opened" << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		std::cout << "INFILE not opened" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+
+	if (argc != 3)
+	{
+		std::cout << "Invalid arguments count\n"
+				  << "Usage fill.exe <input file> <output file>\n";
+
+		return 1;
+	}
+
+	int statusCode = HandleFill(argv[1], argv[2]);
+	if (statusCode != 0)
+	{
+		return 1;
+	}
 
 	return 0;
 }
---
+++ lab1/5_fill/5_fill/test.bat
@@ -0,0 +1,45 @@
+@echo off
+
+set PROGRAM="%~1"
+set OUTPUT="%TEMP%"
+
+rem no params
+%PROGRAM% > nul
+if NOT ERRORLEVEL 1 goto err
+
+rem empty map
+%PROGRAM% tests\empty.txt "%TEMP%\empty-temp-out.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\empty-temp-out.txt" tests\empty-output.txt > nul
+if ERRORLEVEL 1 goto err
+
+rem max
+%PROGRAM% tests\max.txt "%TEMP%\max-temp-out.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\max-temp-out.txt" tests\max-output.txt > nul
+if ERRORLEVEL 1 goto err
+
+rem max bordered
+%PROGRAM% tests\max-bordered.txt "%TEMP%\max-bordered-temp-out.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\max-bordered-temp-out.txt" tests\max-bordered-output.txt > nul
+if ERRORLEVEL 1 goto err
+
+rem multicenters
+%PROGRAM% tests\multicenters.txt "%TEMP%\multicenters-temp-out.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\multicenters-temp-out.txt" tests\multicenters-output.txt > nul
+if ERRORLEVEL 1 goto err
+
+rem out of range
+%PROGRAM% tests\out-of-range.txt "%TEMP%\out-of-range-temp-out.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\out-of-range-temp-out.txt" tests\out-of-range-output.txt > nul
+if ERRORLEVEL 1 goto err
+
+echo Program testing succeeded
+exit 0
+
+:err
+echo Program testing failed
+exit 1
